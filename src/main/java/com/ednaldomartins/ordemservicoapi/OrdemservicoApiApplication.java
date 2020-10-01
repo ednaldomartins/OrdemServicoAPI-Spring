ef873c785685b150e0940752b8fef278cb9f5230
Ednaldo <martinsednaldo@outlook.com>
2020-10-01 14:27:42 -0300

    main.

---
+++ src/main/java/com/ednaldomartins/ordemservicoapi/OrdemservicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ednaldomartins.ordemservicoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemservicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemservicoApiApplication.class, args);
+	}
+
+}
