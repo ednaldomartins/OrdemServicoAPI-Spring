3be0439bd77645e612ba39d40b5884bc9385b5c7
Ednaldo <martinsednaldo@outlook.com>
2020-10-13 10:47:28 -0300

    retornando resposta 404 quando cliente for vazio.

--- src/main/java/com/ednaldomartins/ordemservicoapi/controller/ClienteController.java
+++ src/main/java/com/ednaldomartins/ordemservicoapi/controller/ClienteController.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,19 +30,13 @@ public class ClienteController {
 	}
 	
 	@GetMapping("/clientes/{clienteId}")
-	public Cliente buscarCliente(@PathVariable Long clienteId) {
+	public ResponseEntity<Cliente> buscarCliente(@PathVariable Long clienteId) {
 		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
 		
-		return cliente.orElse(null);
-	}
-	
-	@GetMapping("/clientes/nome/{clienteNome}")
-	public List<Cliente> ListarPorNome(@PathVariable String clienteNome) {
-		return clienteRepository.findByNome(clienteNome);
-	}
-	
-	@GetMapping("/clientes/parteNome/{parteNome}")
-	public List<Cliente> ListarPorNomeContaining(@PathVariable String parteNome) {
-		return clienteRepository.findByNomeContaining(parteNome);
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
