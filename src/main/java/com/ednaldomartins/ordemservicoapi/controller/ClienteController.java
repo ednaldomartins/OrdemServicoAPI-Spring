8f2f516c3596b2199b6228f4f8b99b39302dce91
Ednaldo <martinsednaldo@outlook.com>
2020-10-13 11:20:45 -0300

    Implementação do @PUT do cliente.

--- src/main/java/com/ednaldomartins/ordemservicoapi/controller/ClienteController.java
+++ src/main/java/com/ednaldomartins/ordemservicoapi/controller/ClienteController.java
@@ -7,12 +7,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ednaldomartins.ordemservicoapi.domain.model.Cliente;
@@ -45,7 +49,24 @@ public class ClienteController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizarCliente(
+			@PathVariable Long clienteId, 
+			@RequestBody Cliente cliente
+			) {
+		
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
 	}
 }
