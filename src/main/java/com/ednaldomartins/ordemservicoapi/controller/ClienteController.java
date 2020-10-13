2d27a807356352ebeca5931ec09c3c7408fbb8a0
Ednaldo <martinsednaldo@outlook.com>
2020-10-13 10:41:57 -0300

    Implementando querys para buscar clientes.

--- src/main/java/com/ednaldomartins/ordemservicoapi/controller/ClienteController.java
+++ src/main/java/com/ednaldomartins/ordemservicoapi/controller/ClienteController.java
@@ -1,12 +1,14 @@
 package com.ednaldomartins.ordemservicoapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ednaldomartins.ordemservicoapi.domain.model.Cliente;
@@ -24,5 +26,22 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List<Cliente> Listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public Cliente buscarCliente(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		return cliente.orElse(null);
+	}
+	
+	@GetMapping("/clientes/nome/{clienteNome}")
+	public List<Cliente> ListarPorNome(@PathVariable String clienteNome) {
+		return clienteRepository.findByNome(clienteNome);
+	}
+	
+	@GetMapping("/clientes/parteNome/{parteNome}")
+	public List<Cliente> ListarPorNomeContaining(@PathVariable String parteNome) {
+		return clienteRepository.findByNomeContaining(parteNome);
 	}
 }
