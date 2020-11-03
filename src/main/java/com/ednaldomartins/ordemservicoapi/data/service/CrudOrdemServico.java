ef166dfc95bb0f4d66507a60892f61fbe9ff6ffa
Ednaldo <martinsednaldo@outlook.com>
2020-11-03 16:09:36 -0300

    Implementação: Adicionar comentários a ordem de serviço.

--- src/main/java/com/ednaldomartins/ordemservicoapi/data/service/CrudOrdemServico.java
+++ src/main/java/com/ednaldomartins/ordemservicoapi/data/service/CrudOrdemServico.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ednaldomartins.ordemservicoapi.data.repository.ClienteRepository;
+import com.ednaldomartins.ordemservicoapi.data.repository.ComentarioRepository;
 import com.ednaldomartins.ordemservicoapi.data.repository.OrdemServicoRepository;
 import com.ednaldomartins.ordemservicoapi.domain.exception.NegocioException;
 import com.ednaldomartins.ordemservicoapi.domain.model.Cliente;
+import com.ednaldomartins.ordemservicoapi.domain.model.Comentario;
 import com.ednaldomartins.ordemservicoapi.domain.model.OrdemServico;
 import com.ednaldomartins.ordemservicoapi.domain.model.StatusOrdemServico;
 
@@ -21,6 +23,9 @@ public class CrudOrdemServico {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private ComentarioRepository comentarioRepository;
+	
 	public OrdemServico criar(OrdemServico ordemServico) {
 		Cliente cliente = clienteRepository.findById(
 				ordemServico.getCliente().getId()
@@ -31,5 +36,18 @@ public class CrudOrdemServico {
 		ordemServico.setDataAbertura(OffsetDateTime.now());
 		
 		return ordemServicoRepository.save(ordemServico); 
+	}
+	
+	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
+		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new NegocioException("Ordem de serviço não encontrada"));
+		
+		Comentario comentario = new Comentario(
+				ordemServico,
+				descricao,
+				OffsetDateTime.now()
+				);
+		
+		return comentarioRepository.save(comentario);
 	}
 }
--- src/main/java/com/ednaldomartins/ordemservicoapi/domain/model/OrdemServico.java
+++ src/main/java/com/ednaldomartins/ordemservicoapi/domain/model/OrdemServico.java
@@ -2,6 +2,8 @@ package com.ednaldomartins.ordemservicoapi.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class OrdemServico {
@@ -27,6 +30,10 @@ public class OrdemServico {
 	private StatusOrdemServico status;
 	private OffsetDateTime dataAbertura;
 	private OffsetDateTime dataFinalizacao;
+	
+	//	vincula variavel ordemServico do Objeto Comentario com essa OrdemServico
+	@OneToMany(mappedBy = "ordemServico")
+	private List<Comentario> comentarios = new ArrayList<>();
 	
 	public Long getId() {
 		return id;
@@ -84,6 +91,14 @@ public class OrdemServico {
 		this.dataFinalizacao = dataFinalizacao;
 	}
 	
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
