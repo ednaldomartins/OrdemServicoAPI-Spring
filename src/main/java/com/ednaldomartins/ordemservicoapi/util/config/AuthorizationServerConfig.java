0792d9c3dcd5cc7260ef217990676fc506ed8ebe
Ednaldo <martinsednaldo@outlook.com>
2020-11-13 13:51:30 -0300

    Implementação: Access Token e Refresh Token.

--- src/main/java/com/ednaldomartins/ordemservicoapi/util/config/AuthorizationServerConfig.java
+++ src/main/java/com/ednaldomartins/ordemservicoapi/util/config/AuthorizationServerConfig.java
@@ -8,7 +8,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -23,18 +24,29 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient("spring")
 			.secret("spring123")
 			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(2000);
+			.authorizedGrantTypes("password", "refresh_token")
+			.accessTokenValiditySeconds(60)
+			.refreshTokenValiditySeconds(60*60*24);
 	}
 	
 	@Override
 	public void configure(org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+		endpoints.tokenStore(tokenStore())
+			.accessTokenConverter(accessTokenConverter())
+			.reuseRefreshTokens(false)
+			.authenticationManager(authenticationManager);
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setSigningKey("springboot");
+		return jwtAccessTokenConverter;
 	}
 	
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 	
 }
