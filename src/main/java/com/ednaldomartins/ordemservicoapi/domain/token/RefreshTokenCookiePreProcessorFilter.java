582a1ed221a38bd7f1fecb740b078d69540e50e6
Ednaldo <martinsednaldo@outlook.com>
2020-11-13 16:26:14 -0300

    Implementação: Gerar refresh_token, guardá-lo no cookie, e usá-lo para gerar novos access_token.

---
+++ src/main/java/com/ednaldomartins/ordemservicoapi/domain/token/RefreshTokenCookiePreProcessorFilter.java
@@ -0,0 +1,68 @@
+package com.ednaldomartins.ordemservicoapi.domain.token;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.catalina.util.ParameterMap;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RefreshTokenCookiePreProcessorFilter implements Filter {
+
+	@Override
+	public void doFilter(
+			ServletRequest request,
+			ServletResponse response, 
+			FilterChain chain
+	) throws IOException, ServletException {
+		HttpServletRequest servletRequest = (HttpServletRequest) request;
+		
+		boolean isEqualsAndContain = 
+				"/oauth/token".equalsIgnoreCase(servletRequest.getRequestURI()) &&
+				"refresh_token".equals(servletRequest.getParameter("grant_type")) &&
+				servletRequest.getCookies() != null;
+		
+		if (isEqualsAndContain) {
+			for (Cookie cookie: servletRequest.getCookies()) {
+				if (cookie.getName().equals("refreshToken")) {
+					String refreshToken = cookie.getValue();
+					servletRequest = new MyServletRequestWrapper(servletRequest, refreshToken);
+					break;
+				}
+			}
+		}
+		
+		chain.doFilter(servletRequest, response);
+	}
+	
+	static class MyServletRequestWrapper extends HttpServletRequestWrapper {
+		
+		private String refreshToken;
+		
+		public MyServletRequestWrapper(HttpServletRequest request, String refreshToken) {
+			super(request);
+			this.refreshToken = refreshToken;
+		}
+		
+		@Override
+		public Map<String, String[]> getParameterMap() {
+			ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
+			map.put("refresh_token", new String [] {refreshToken} );
+			map.setLocked(true);
+			return map;
+		}
+	}
+
+}
---
+++ src/main/java/com/ednaldomartins/ordemservicoapi/domain/token/RefreshTokenPostProcessor.java
@@ -0,0 +1,53 @@
+package com.ednaldomartins.ordemservicoapi.domain.token;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken>{
+
+	@Override
+	public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+		return returnType.getMethod().getName().equals("postAccessToken");
+	}
+
+	@Override
+	public OAuth2AccessToken beforeBodyWrite(
+			OAuth2AccessToken body,
+			MethodParameter returnType,
+			MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
+			ServerHttpRequest request, ServerHttpResponse response
+	) {
+		String refreshToken = body.getRefreshToken().getValue();
+		HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+		HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+		
+		//	guardando refresh token no cookie
+		Cookie cookie = new Cookie("refreshToken", refreshToken);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(false); // http -> usar https em producao
+		cookie.setPath(servletRequest.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(60*60*24*30);
+		servletResponse.addCookie(cookie);
+		
+		//	apagando refresh token do body
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+		token.setRefreshToken(null);
+		
+		return body;
+	}
+
+}
