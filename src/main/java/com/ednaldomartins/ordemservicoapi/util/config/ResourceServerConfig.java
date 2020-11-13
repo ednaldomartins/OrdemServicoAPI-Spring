cf36b5e1e864fe27f7392c27aadc774445512503
Ednaldo <martinsednaldo@outlook.com>
2020-11-13 10:25:03 -0300

    Implementação: Autenticação com OAuth2.

--- src/main/java/com/ednaldomartins/ordemservicoapi/util/config/ResourceServerConfig.java
+++ src/main/java/com/ednaldomartins/ordemservicoapi/util/config/ResourceServerConfig.java
@@ -1,25 +1,29 @@
 package com.ednaldomartins.ordemservicoapi.util.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 //@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
-	@Override
+	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("admin")
-			.password("{noop}admin")
+			.password("admin")
 			.roles("role");
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			/*	permitAll() para permitir o acesso a esse caminho sem auth 
 			 *	/ordens-servico/* para bloquear 0 ou mais caracteres
@@ -27,11 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers("/ordens-servico").permitAll()	*/
 			.anyRequest().authenticated()
 			.and()
-			.httpBasic()
-			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.csrf().disable();	// desabilitado enquanto nao tem serviço web	
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
 	}
 
 }
