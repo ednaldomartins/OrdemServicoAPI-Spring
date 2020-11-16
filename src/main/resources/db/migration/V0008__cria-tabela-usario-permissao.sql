2c41d343feb2bf92d03b3e37db4d967f438ce136
Ednaldo <martinsednaldo@outlook.com>
2020-11-16 15:00:39 -0300

    Implementação: Cria tabela (JOIN) de permissão_usuário.

---
+++ src/main/resources/db/migration/V0008__cria-tabela-usario-permissao.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuario_permissao (
+	id_usuario BIGINT(20) NOT NULL,
+	id_permissao BIGINT(20) NOT NULL,
+	PRIMARY KEY (id_usuario, id_permissao),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+	FOREIGN KEY (id_permissao) REFERENCES permissao(id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
