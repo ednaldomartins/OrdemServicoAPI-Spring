013cc25adb67910a426104d84d9454165a028133
Ednaldo <martinsednaldo@outlook.com>
2020-11-16 15:01:32 -0300

    Implementação: Insere tipos de permissões e usuários testes do sistema.

---
+++ src/main/resources/db/migration/V0009__insere-permissoes-usuario.sql
@@ -0,0 +1,11 @@
+INSERT INTO permissao (id, descricao) values (1, 'ROLE_CADASTRAR_CLIENTE');
+INSERT INTO permissao (id, descricao) values (2, 'ROLE_REMOVER_CLIENTE');
+INSERT INTO permissao (id, descricao) values (3, 'ROLE_PESQUISAR_CLIENTE');
+
+INSERT INTO permissao (id, descricao) values (4, 'ROLE_CADASTRAR_ORDEM_SERVICO');
+INSERT INTO permissao (id, descricao) values (5, 'ROLE_REMOVER_ORDEM_SERVICO');
+INSERT INTO permissao (id, descricao) values (6, 'ROLE_PESQUISAR_ORDEM_SERVICO');
+
+INSERT INTO permissao (id, descricao) values (7, 'ROLE_CADASTRAR_COMENTARIO');
+INSERT INTO permissao (id, descricao) values (8, 'ROLE_PESQUISAR_COMENTARIO');
+
---
+++ src/main/resources/db/migration/V0010__insere-usuarios-testes.sql
@@ -0,0 +1,29 @@
+-- usuarios
+INSERT INTO usuario (nome, email, senha) values ('Administrador', 'admin@os.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+INSERT INTO usuario (nome, email, senha) values ('Funcionario 1', 'funcionario1@os.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+INSERT INTO usuario (nome, email, senha) values ('Cliente 1', 'cliente1@email.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
+
+-- admin
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 1);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 2);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 3);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 4);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 5);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 6);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 7);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (1, 8);
+
+-- funcionario
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 1);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 3);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 4);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 6);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 7);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (2, 8);
+
+-- cliente
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (3, 1);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (3, 3);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (3, 6);
+INSERT INTO usuario_permissao (id_usuario, id_permissao) values (3, 8);
+
